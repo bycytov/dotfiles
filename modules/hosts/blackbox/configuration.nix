f0756dd348ff41fb2c4ff67b053a9e9819a81a6e
bycytov <bycytov@gmail.com>
2026-03-13 12:43:15 -0500

    init den refactor

---
+++ modules/hosts/blackbox/configuration.nix
@@ -0,0 +1,87 @@
+{ den, ...}: {
+  # host aspect
+  den.aspects.blackbox = {
+    # host NixOS configuration
+    nixos =
+      { config, lib, pkgs, ... }:
+      {
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.efi.canTouchEfiVariables = true;
+
+        networking = {
+          firewall.enable = false;
+          hostName = "blackbox";
+          nftables.enable = true;
+          interfaces = {
+            br0 = {
+              useDHCP = false;
+              ipv4.addresses = [
+                {
+                  address = "192.168.1.3";
+                  prefixLength = 20;
+                }
+              ];
+            };
+          };
+          bridges = {
+            br0 = {
+              interfaces = [ "enp3s0" ];
+            };
+          };
+          defaultGateway = "192.168.1.1";
+          nameservers = [ "192.168.1.1" ];
+        };
+
+        time.timeZone = "America/Chicago";
+
+        users.users.sam = {
+          isNormalUser = true;
+          extraGroups = [
+            "wheel"
+            "incus-admin"
+            "video"
+            "render"
+          ]; 
+        };
+
+        environment.systemPackages = with pkgs; [
+          btop
+          curl
+          fastfetch
+          iotop
+          intel-gpu-tools
+          mergerfs
+          ncdu
+          tmux
+          vim
+          wget
+        ];
+
+        services.openssh.enable = true;
+        services.getty.autologinUser = "sam";
+        services.tailscale.enable = true;
+
+        nix = {
+          settings = {
+            experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+            warn-dirty = true;
+          };
+          gc = {
+            automatic = true;
+            dates = "weekly";
+            options = "--delete-older-than 7d";
+          };
+        };
+      };
+
+    # host provides default home environment for its users
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.tree ];
+      };
+  };
+}
---
+++ modules/sam.nix
@@ -0,0 +1,37 @@
+{ den, ... }:
+{
+  # user aspect
+  den.aspects.sam = {
+    includes = [
+      den.provides.primary-user
+      (den.provides.user-shell "bash")
+    ];
+
+    homeManager =
+      { pkgs, ... }:
+      {
+        programs.git = {
+          enable = true;
+          settings = {
+            user = {
+              name = "bycytov";
+              email = "bycytov@gmail.com";
+            };
+            init.defaultBranch = "main";
+          }; 
+        };
+        programs.bash = {
+        #############  enable = true;
+          shellAliases = {
+            lg = "lazygit";
+          };
+        };
+        programs.lazygit.enable = true;
+        home.packages = [ pkgs.htop ];
+      };
+
+    # user can provide NixOS configurations
+    # to any host it is included on
+    # nixos = { pkgs, ... }: { };
+  };
+}
