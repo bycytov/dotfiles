8136378984ee9aef4cbc94c7adfefe8f17bb89d5
bycytov <bycytov@gmail.com>
2026-03-17 16:18:36 -0500

    update rest of inputs
    nux run .#blackbox -- switch no longer works

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 # DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
-
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
@@ -9,13 +8,13 @@
     flake-aspects.url = "github:vic/flake-aspects";
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
     };
     helix.url = "github:helix-editor/helix";
     home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:vic/import-tree";
     lazyvim.url = "github:pfassina/lazyvim-nix";
@@ -23,5 +22,4 @@
     nixpkgs-lib.follows = "nixpkgs";
     nixpkgs-stable.url = "nixpkgs/nixos-25.11";
   };
-
 }
