4dadcfb3e84cf8751aad52e1f07a55321f795edb
bycytov <bycytov@gmail.com>
2026-03-26 11:27:27 -0500

    enable bash for all users

--- modules/core/defaults.nix
+++ modules/core/defaults.nix
@@ -9,6 +9,8 @@
       den._.hostname
       # Automatically create users.users.<name> on OS + home.username in HM
       den._.define-user
+      # set default shell for all users
+      ( den._.user-shell "bash" )
       # Expose system-specialised inputs' and self' as module args
       den._.inputs'
       den._.self'
